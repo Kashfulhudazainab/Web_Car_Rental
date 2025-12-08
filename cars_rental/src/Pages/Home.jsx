c0a16fb6e0d17ed6ea48940b29643cdaa9b99c1b
Huda <huda@gmail.com>
2025-12-08 14:05:39 +0500

    Added responsive Testimonials section with carousel and dark-mode support

--- cars_rental/src/Pages/Home.jsx
+++ cars_rental/src/Pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Hero from '../Components/home/Hero'
+import Testimonials from "../Components/Testimonials";
+
+
+
 
 function Home() {
     return (
         <div>
            <Navbar/> 
            <Hero/>
+           <Testimonials />
+
         </div>
     )
 }
